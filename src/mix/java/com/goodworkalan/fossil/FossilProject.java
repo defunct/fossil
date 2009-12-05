136b102f5a846eab8c2b85f9eb8949b3409604e6
Alan Gutierrez <alan@blogometer.com>
2009-12-05 05:41:35 -0600

    Mix build.

---
+++ src/mix/java/com/goodworkalan/fossil/FossilProject.java
@@ -0,0 +1,29 @@
+package com.goodworkalan.strata.mix;
+
+import com.goodworkalan.go.go.Artifact;
+import com.goodworkalan.mix.ProjectModule;
+import com.goodworkalan.mix.builder.Builder;
+import com.goodworkalan.mix.builder.JavaProject;
+
+public class FossilProject extends ProjectModule {
+    @Override
+    public void build(Builder builder) {
+        builder
+            .cookbook(JavaProject.class)
+                .produces(new Artifact("com.goodworkalan/fossil/0.1"))
+                .main()
+                    .depends()
+                        .artifact(new Artifact("com.goodworkalan/strata/0.1"))
+                        .artifact(new Artifact("com.goodworkalan/pack/0.1"))
+                        .end()
+                    .end()
+                .test()
+                    .depends()
+                        .artifact(new Artifact("org.testng/testng/5.10/jdk15"))
+                        .artifact(new Artifact("org.mockito/mockito-core/1.6"))
+                        .end()
+                    .end()
+                .end()
+            .end();
+    }
+}
